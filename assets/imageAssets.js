42024a8b2948ec0762edca99c3bd8579601e3405
Dryone <andriawan787@gmail.com>
2023-01-30 15:10:07 +0700

    login register UI

--- assets/imageAssets.js
+++ assets/imageAssets.js
@@ -2,4 +2,5 @@ export const ImageAssets = Object.freeze({
     logo: require('./images/logo.jpg'),
     foodLogin: require('./images/food-login.png'),
     foodRegister: require('./images/food-register.png'),
+    logoTextPutih: require('./images/logo-text-putih.png'),
 });
